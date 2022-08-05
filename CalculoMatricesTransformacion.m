bcf302701868e3f1ee40386e503a3a83822607ca
PabloCBEG <pablocarben@outlook.com>
2022-08-05 13:50:08 +0200

    Graphic validation test for DH model

--- CalculoMatricesTransformacion.m
+++ CalculoMatricesTransformacion.m
@@ -2,17 +2,19 @@
 
 tabla = tablaDH();
 
-theta = [tabla(2,1) tabla(3,1) tabla(4,1) tabla(5,1) tabla(6,1) tabla(7,1)]; %Empezamos por la fila 2, pues la 1 (primera fila de la tabla) se corresponde con la base fija
-d     = [tabla(2,2) tabla(3,2) tabla(4,2) tabla(5,2) tabla(6,2) tabla(7,2)];
-a     = [tabla(2,3) tabla(3,3) tabla(4,3) tabla(5,3) tabla(6,3) tabla(7,3)];
-alpha = [tabla(2,4) tabla(3,4) tabla(4,4) tabla(5,4) tabla(6,4) tabla(7,4)];
+theta = [tabla(1,1) tabla(2,1) tabla(3,1) tabla(4,1) tabla(5,1) tabla(6,1) tabla(7,1)]; %Empezamos por la fila 2, pues la 1 (primera fila de la tabla) se corresponde con la base fija
+d     = [tabla(1,2) tabla(2,2) tabla(3,2) tabla(4,2) tabla(5,2) tabla(6,2) tabla(7,2)];
+a     = [tabla(1,3) tabla(2,3) tabla(3,3) tabla(4,3) tabla(5,3) tabla(6,3) tabla(7,3)];
+alpha = [tabla(1,4) tabla(2,4) tabla(3,4) tabla(4,4) tabla(5,4) tabla(6,4) tabla(7,4)];
 
-a01 = simplify(trotz(theta(1))*transl([0 0 d(1)])*transl([a(1) 0 0])*trotx(alpha(1)));
-a12 = simplify(trotz(theta(2))*transl([0 0 d(2)])*transl([a(2) 0 0])*trotx(alpha(2)));
-a23 = simplify(trotz(theta(3))*transl([0 0 d(3)])*transl([a(3) 0 0])*trotx(alpha(3)));
-a34 = simplify(trotz(theta(4))*transl([0 0 d(4)])*transl([a(4) 0 0])*trotx(alpha(4)));
-a45 = simplify(trotz(theta(5))*transl([0 0 d(5)])*transl([a(5) 0 0])*trotx(alpha(5)));
-a56 = simplify(trotz(theta(6))*transl([0 0 d(6)])*transl([a(6) 0 0])*trotx(alpha(6)));
+ab0 = simplify(trotz(theta(1))*transl([0 0 d(1)])*transl([a(1) 0 0])*trotx(alpha(1)));
+
+a01 = simplify(trotz(theta(2))*transl([0 0 d(2)])*transl([a(2) 0 0])*trotx(alpha(2)));
+a12 = simplify(trotz(theta(3))*transl([0 0 d(3)])*transl([a(3) 0 0])*trotx(alpha(3)));
+a23 = simplify(trotz(theta(4))*transl([0 0 d(4)])*transl([a(4) 0 0])*trotx(alpha(4)));
+a34 = simplify(trotz(theta(5))*transl([0 0 d(5)])*transl([a(5) 0 0])*trotx(alpha(5)));
+a45 = simplify(trotz(theta(6))*transl([0 0 d(6)])*transl([a(6) 0 0])*trotx(alpha(6)));
+a56 = simplify(trotz(theta(7))*transl([0 0 d(7)])*transl([a(7) 0 0])*trotx(alpha(7)));
 
 T06 = simplify(a01*a12*a23*a34*a45*a56);
 
@@ -24,6 +26,8 @@ theta3 = [tabla3(2,1) tabla3(3,1) tabla3(4,1)];
 d3     = [tabla3(2,2) tabla3(3,2) tabla3(4,2)];
 a3     = [tabla3(2,3) tabla3(3,3) tabla3(4,3)];
 alpha3 = [tabla3(2,4) tabla3(3,4) tabla3(4,4)];
+
+ab0_3 = ab0;
 
 a01_3 = simplify(trotz(theta3(1))*transl([0 0 d3(1)])*transl([a3(1) 0 0])*trotx(alpha3(1)));
 a12_3 = simplify(trotz(theta3(2))*transl([0 0 d3(2)])*transl([a3(2) 0 0])*trotx(alpha3(2)));
---
+++ comprobacionGrafica.m
@@ -0,0 +1,35 @@
+L0 = 1;
+L1 = 0.5;
+L2 = 1;
+L3A = 1;
+L3B = 0.5;
+L3 = sqrt(L3A^2 + L3B^2);
+L4 = 1.5;
+L5 = 1.5;
+L6 = 0.5;
+
+clear L
+%             th        d       a       alpha
+L(1) = Link([ pi/2      L0      0       pi/2        0], 'standard');
+L(2) = Link([ pi/2      0       0       pi/2        0], 'standard');
+L(3) = Link([ 0         L1      L2      -pi/2       0], 'standard');
+L(4) = Link([ pi/2      L3A    	-L3B    -pi/2       0], 'standard');
+L(5) = Link([ pi        L4      0       pi/2        0], 'standard');
+L(6) = Link([ 0         L5      0       pi/2        0], 'standard');
+L(7) = Link([ 0         L6      0       0           0], 'standard');
+%Antes de introducir L(1) para la base (con las coordenadas articulares de
+%la base)//tal y como está definido ahora, la configuración del robot es
+%correcta, pero está mal orientada: está construido como si la articulación
+%1 fuese de rotación respecto del eje vertical, cuando es respecto del eje
+%horizontal. Seguramente falle yo al definir algo.
+%Una vez introducida L(1) con la base//ahora funciona. La representación
+%gráfica del robot coincide con lo que tenemos: como conclusión, importante
+%tener en cuenta la base cuando queremos coordenadas absolutas (era fácil
+%de intuir, pero bueno).
+
+proyecto = SerialLink(L, 'name', 'Manipulador proyecto','manufacturer','Dpto. Sistemas y Automática - ETSI - US');
+
+qz = [0 0 0 0 0 0]; % zero angles
+qr = [0 pi/2 0 pi/2 pi 0 0]; % postura dibujo
+
+proyecto.plot(qr,'jointdiam',1,'jaxes');
